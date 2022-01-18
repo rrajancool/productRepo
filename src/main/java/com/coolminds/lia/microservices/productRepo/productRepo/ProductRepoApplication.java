4adb1cd4c9dbbd2da8e20444e7938ab6298b82b3
Rathish Rajan <rrajan@incomm.com>
2022-01-18 13:08:40 +0530

    first commit

---
+++ src/main/java/com/coolminds/lia/microservices/productRepo/productRepo/ProductRepoApplication.java
@@ -0,0 +1,13 @@
+package com.coolminds.lia.microservices.productRepo.productRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coolminds/lia/microservices/productRepo/productRepo/ProductRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolminds.lia.microservices.productRepo.productRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
